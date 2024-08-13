81cf0feaba2ad10d3293cf1e44b17c83271d4c7a
Twilightdashxo <120600068+Twilightdashxo@users.noreply.github.com>
2024-08-13 21:19:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+    const num = document.getElementById("n");
+    num.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+            console.log(num.value);
+            let n = num.value;
+            if ((n % 3 == 0) && (n % 5 !== 0)) {
+                document.getElementById("output").innerHTML = "Fizz";
+            } else if ((n % 5 == 0) && (n % 3 !== 0)) {
+                document.getElementById("output").innerHTML = "Buzz";
+            } else if ((n % 3 == 0) && (n % 5 == 0)) {
+                document.getElementById("output").innerHTML = "FizzBuzz";
+            } else {
+                document.getElementById("output").innerHTML = "";
+            }
+        }
+    });
